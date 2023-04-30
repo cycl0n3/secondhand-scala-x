1ad5cd821e64135da32b41c8b561b73e2f1d2dc9
Milind <7milind7@gmail.com>
2023-04-30 14:45:15 +0530

    more improvements

--- app/src/main/java/com/secondhand/role/RoleController.java
+++ app/src/main/java/com/secondhand/role/RoleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,7 +32,8 @@ public class RoleController {
     public ResponseEntity<Map<String, Object>> getRoles() {
         Map<String, Object> response = new HashMap<>();
 
-        response.put("roles", roleMapper.toRoleDto(roleService.getRoles()));
+        List<Role> roles = roleService.getRoles();
+        response.put("roles", roleMapper.toRoleDto(roles));
 
         return ResponseEntity.ok(response);
     }
--- app/src/main/java/com/secondhand/user/UserController.java
+++ app/src/main/java/com/secondhand/user/UserController.java
@@ -1,6 +1,7 @@
 package com.secondhand.user;
 
 import com.secondhand.exception.GenericException;
+
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,13 +22,22 @@ public class UserController {
 
     private final UserMapper userMapper;
 
-    @GetMapping("/all")
+    @GetMapping
     public ResponseEntity<Map<String, Object>> getAllUsers() {
         List<User> users = userService.getAllUsers();
 
         Map<String, Object> response = new HashMap<>();
-
         response.put("users", userMapper.toUserDto(users));
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<Map<String, Object>> getUserById(Long userId) {
+        User user = userService.getUserById(userId);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("user", userMapper.toUserDto(user));
 
         return ResponseEntity.ok(response);
     }
